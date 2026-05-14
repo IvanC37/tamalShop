373f37c683c72b6bbdc7016b79790dab2a59a677
Ivan Castillo <castillo.ivan.2033@eam.edu.co>
2026-05-13 23:34:24 -0500

    Conexion Railway PostgreSQL

--- conexion.php
+++ conexion.php
@@ -1,18 +1,12 @@
 <?php
 
-$host = "localhost";
-$port = "5432";
-$dbname = "tamalshop";
-$user = "postgres";
-$password = "1192";
-
 $conn = pg_connect("
-    host=$host
-    port=$port
-    dbname=$dbname
-    user=$user
-    password=$password
-");
+    host=" . getenv("PGHOST") . "
+    port=" . getenv("PGPORT") . "
+    dbname=" . getenv("PGDATABASE") . "
+    user=" . getenv("PGUSER") . "
+    password=" . getenv("PGPASSWORD")
+);
 
 if (!$conn) {
     die("Error de conexión con PostgreSQL");
