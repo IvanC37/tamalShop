602b5d283387a766cab403f0f422c56b0de1d96e
emdi5267 <emilio.diaz.5267@eam.edu.co>
2026-05-14 18:36:14 -0500

    Configuracion conexion 2

--- conexion.php
+++ conexion.php
@@ -1,15 +1,25 @@
 <?php
 
-$conn = pg_connect(
-    "host=" . getenv("PGHOST") .
-    " port=" . getenv("PGPORT") .
-    " dbname=" . getenv("PGDATABASE") .
-    " user=" . getenv("PGUSER") .
-    " password=" . getenv("PGPASSWORD")
-);
+$host = getenv("PGHOST");
+$port = getenv("PGPORT");
+$dbname = getenv("PGDATABASE");
+$user = getenv("PGUSER");
+$password = getenv("PGPASSWORD");
+
+$conn_string = "
+    host=$host
+    port=$port
+    dbname=$dbname
+    user=$user
+    password=$password
+";
+
+$conn = pg_connect($conn_string);
 
 if (!$conn) {
+
     die("Error de conexión con PostgreSQL");
+
 }
 
 ?>
